5dedd51ab3fe0e3bcdb8e0ef29adba9a8b25459a
Aakash Murari <aakash_murari@hotmail.com>
2023-12-18 00:49:09 -0600

    Added Day4B

--- src/main/java/Day4.java
+++ src/main/java/Day4.java
@@ -1,22 +1,40 @@
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class Day4 {
-    public long solveA(List<Day4Input> cards) {
+    int[] cardCounts = new int[500];
+
+    public long solveA(List<Card> cards) {
         long output = 0;
-        for (Day4Input card : cards) {
-            Set<Integer> winningNumbers = new HashSet<>(card.winningNumbers());
-            int count = 0;
-            for (Integer myNumber : card.myNumbers()) {
-                if (winningNumbers.contains(myNumber)) count++;
-            }
-            if(count>0) output += 1L << (count-1);
+        for (Card card : cards) {
+            int count = getWinningCount(card);
+            if (count > 0) output += 1L << (count - 1);
 //            System.out.println((1L << (count-1)) + " : " + output);
         }
         return output;
     }
+
+    private int getWinningCount(Card card) {
+        Set<Integer> winningNumbers = new HashSet<>(card.winningNumbers());
+        int count = 0;
+        for (Integer myNumber : card.myNumbers()) {
+            if (winningNumbers.contains(myNumber)) count++;
+        }
+        return count;
+    }
+
+    public long solveB(List<Card> cards) {
+        for (Card card : cards) cardCounts[card.cardNumber()] = 1;
+        for (Card card : cards) {
+            int count = getWinningCount(card);
+            while (count > 0) {
+                cardCounts[card.cardNumber() + count--] += cardCounts[card.cardNumber()];
+            }
+        }
+
+        return IntStream.of(cardCounts).sum();
+    }
 }
 
-record Day4Input(List<Integer> winningNumbers, List<Integer> myNumbers) {
+record Card(Integer cardNumber, List<Integer> winningNumbers, List<Integer> myNumbers) {
 }
--- src/test/java/Day4Test.java
+++ src/test/java/Day4Test.java
@@ -18,9 +18,10 @@ public class Day4Test {
         Assertions.assertEquals(21105, output);
     }
 
-    List<Day4Input> getInput(String file) {
+    List<Card> getInput(String file) {
         List<String> lines = TestUtil.readInput(file);
-        List<Day4Input> input = new ArrayList<>();
+        List<Card> input = new ArrayList<>();
+        int cardNumber = 1;
         for (String line : lines) {
             Scanner scanner = new Scanner(line);
             scanner.next();
@@ -32,21 +33,21 @@ public class Day4Test {
             List<Integer> myNumbers = new ArrayList<>();
             while(scanner.hasNextInt()) myNumbers.add(scanner.nextInt());
 
-            input.add(new Day4Input(winningNumbers, myNumbers));
+            input.add(new Card(cardNumber++, winningNumbers, myNumbers));
         }
         return input;
     }
 
-//    @Test
-//    void solveExampleB() {
-//        int output = new Day3().solveB(TestUtil.readInput("day3-example.txt"));
-//        Assertions.assertEquals(467835, output);
-//    }
-//
-//    @Test
-//    void solveInputB() {
-//        int output = new Day3().solveB(TestUtil.readInput("day3-input.txt"));
-//        Assertions.assertEquals(80403602, output);
-//    }
+    @Test
+    void solveExampleB() {
+        long output = new Day4().solveB(getInput("day4-example.txt"));
+        Assertions.assertEquals(30, output);
+    }
+
+    @Test
+    void solveInputB() {
+        long output = new Day4().solveB(getInput("day4-input.txt"));
+        Assertions.assertEquals(5329815, output);
+    }
 }
 
