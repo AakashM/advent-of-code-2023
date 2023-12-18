f389af3f31dcbb7beb2304f165024a5abe4c3022
Aakash Murari <aakash_murari@hotmail.com>
2023-12-17 22:59:49 -0600

    Added day 3 B

--- src/main/java/Advent3A.java
+++ src/main/java/Advent3A.java
@@ -1,23 +1,25 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Advent3A {
-    public int solve(List<String> strings) {
-        Pattern pattern = Pattern.compile("\\d+");
+    Map<Gear, List<Integer>> gears = new HashMap<>();
+    Pattern NUMBER_PATTERN = Pattern.compile("\\d+");
 
+    public int solveA(List<String> strings) {
         int output = 0;
-        for (int i = 0; i < strings.size(); i++) {
-            String str = strings.get(i);
-            Matcher matcher = pattern.matcher(str);
+        for (int r = 0; r < strings.size(); r++) {
+            String str = strings.get(r);
+            Matcher matcher = NUMBER_PATTERN.matcher(str);
 
             while (matcher.find()) {
-                if (isAdjacentToSymbol(i, matcher.start(), matcher.end() - 1, strings)) {
+                if (!getAdjacentGears(r, matcher.start(), matcher.end() - 1, strings).isEmpty()) {
                     output += Integer.parseInt(matcher.group());
 
                     System.out.println(matcher.group());
-//                    System.out.println(matcher.start());
-//                    System.out.println(matcher.end());
                 }
             }
         }
@@ -25,15 +27,56 @@ public class Advent3A {
         return output;
     }
 
-    private boolean isAdjacentToSymbol(int idx, int start, int end, List<String> strings) {
-        var symbolPat = Pattern.compile(".*?[^0-9.].*").pattern();
-        String currentLine = strings.get(idx);
+    private List<Gear> getAdjacentGears(int r, int start, int end, List<String> strings) {
+        List<Gear> output = new ArrayList<>();
+        var symbolPattern = Pattern.compile(".*?[^0-9.].*").pattern();
+        String currentLine = strings.get(r);
+
         int beforeStart = Math.max(0, start - 1);
         int afterEnd = Math.min(currentLine.length() - 1, end + 1);
-        return ((start - 1 >= 0 && currentLine.substring(start - 1, start).matches(symbolPat))
-                || (end + 1 < currentLine.length() && currentLine.substring(end + 1, end + 2).matches(symbolPat))
-                || idx > 0 && strings.get(idx - 1).substring(beforeStart, afterEnd + 1).matches(symbolPat)
-                || idx < currentLine.length() - 1 && strings.get(idx + 1).substring(beforeStart, afterEnd + 1).matches(symbolPat)
-        );
+        if (start - 1 >= 0 && currentLine.substring(start - 1, start).matches(symbolPattern))
+            output.add(new Gear(r, start-1));
+        if (end + 1 < currentLine.length() && currentLine.substring(end + 1, end + 2).matches(symbolPattern))
+            output.add(new Gear(r, end+1));
+        if(r>0) {
+            for(int c=beforeStart; c<=afterEnd; c++) {
+                if(strings.get(r - 1).substring(c, c + 1).matches(symbolPattern))
+                    output.add(new Gear(r-1, c));
+            }
+        }
+        if(r<currentLine.length() - 1) {
+            for(int c=beforeStart; c<=afterEnd; c++) {
+                if(strings.get(r + 1).substring(c, c + 1).matches(symbolPattern))
+                    output.add(new Gear(r+1, c));
+            }
+        }
+        return output;
     }
+
+    public int solveB(List<String> strings) {
+
+        int output = 0;
+        for (int r = 0; r < strings.size(); r++) {
+            String str = strings.get(r);
+            Matcher matcher = NUMBER_PATTERN.matcher(str);
+
+            while (matcher.find()) {
+                for (Gear gear : getAdjacentGears(r, matcher.start(), matcher.end() - 1, strings)) {
+                    gears.putIfAbsent(gear, new ArrayList<>());
+                    gears.get(gear).add(Integer.parseInt(matcher.group()));
+                }
+            }
+        }
+
+        for (List<Integer> nums : gears.values()) {
+            if(nums.size()==2) {
+                output += nums.get(0) * nums.get(1);
+            }
+        }
+
+        return output;
+    }
+}
+
+record Gear(int r, int c) {
 }
--- src/test/java/Advent3ATest.java
+++ src/test/java/Advent3ATest.java
@@ -3,15 +3,27 @@ import org.junit.jupiter.api.Test;
 
 public class Advent3ATest {
     @Test
-    void solveExA() {
-        int output = new Advent3A().solve(TestInput.readInput("advent3ex.txt"));
+    void solveExampleA() {
+        int output = new Advent3A().solveA(TestInput.readInput("advent3ex.txt"));
         Assertions.assertEquals(4361, output);
     }
 
     @Test
     void solveInputA() {
-        int output = new Advent3A().solve(TestInput.readInput("advent3input.txt"));
+        int output = new Advent3A().solveA(TestInput.readInput("advent3input.txt"));
         Assertions.assertEquals(528819, output);
+    }
+
+    @Test
+    void solveExampleB() {
+        int output = new Advent3A().solveB(TestInput.readInput("advent3ex.txt"));
+        Assertions.assertEquals(467835, output);
+    }
+
+    @Test
+    void solveInputB() {
+        int output = new Advent3A().solveB(TestInput.readInput("advent3input.txt"));
+        Assertions.assertEquals(80403602, output);
     }
 }
 
